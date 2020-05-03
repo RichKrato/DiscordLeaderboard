9b4a9d96351056510f399b4b22702cfbd2f3ceea
ThatBogen <googlandia4@gmail.com>
2020-05-03 18:29:14 +0200

    New command

--- src/main/java/cz/sspbrno/config.java
+++ src/main/java/cz/sspbrno/config.java
@@ -1,10 +1,10 @@
 package cz.sspbrno;
 
 public class config {
-    public static String db_user = "";
-    public static String db_passwd = "";
+    public static String db_user = "csgd";
+    public static String db_passwd = "QwcjD7kyWRzLjUvz";
 
-    public static String discord_token = "";
+    public static String discord_token = "Njc1MzAzMzgyMjE4NzAyOTI5.Xq6xGA.ZjJss0uVOSE1H9U86hBcRIjbGw8";
     public static String discord_activity = "type -/commands";
 }
 
